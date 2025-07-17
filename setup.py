bd8c301e920dbdeef66fb4503a7803cce5b32471
quantatrisk <pengzhia@gmail.com>
2025-07-17 11:24:27 +0800

    Bump version to 1.0.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="doc2txt",
-    version="1.0.3",
+    version="1.0.4",
     packages=find_packages(),
     include_package_data=True,
     package_data={
